b0b3d3a7d5a1c766ceb4f63da2a92fba82fc9c4f
David Murphy <murphy.davidj@gmail.com>
2015-05-28 15:09:14 -0400

    move instructions

---
+++ teradata-driver-installation/README.md
@@ -0,0 +1,5 @@
+Install Teradata JDBC driver as a core module in JBoss Data Virtualization
+
+Step 1
+======
+Unzip 
---
+++ teradata-driver-installation/driver.bat
@@ -0,0 +1,1 @@
+jboss-cli.bat -c "/subsystem=datasources/jdbc-driver=teradata:add(driver-name=teradata,driver-module-name=com.teradata,driver-class-name=com.teradata.jdbc.TeraDriver)"
